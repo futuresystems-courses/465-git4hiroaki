a971af4edbee55f56a7b961af2f44528bb28bd50
Hiroaki Shioi <shioin@hotmail.com>
2015-05-28 15:59:57 -0400

    create Archive dir for storing other files

---
+++ Archive/default_portal.sh
@@ -0,0 +1,10 @@
+export PORTALNAME=hshioi
+export PROJECTID=fg465
+
+module load openstack
+#module load python\2.7
+
+source .cloudmesh/clouds/india/juno/fg465
+source ~/.cloudmesh/clouds/india/juno/openrc.sh
+
+
